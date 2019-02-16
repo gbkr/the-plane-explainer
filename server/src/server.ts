b20cdf1ddd9f97229caa448e5d6f712a2fab72d0
Greg Baker <greg@platform45.com>
2019-02-16 19:23:37 +0000

    clean up

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,17 +20,3 @@ app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}/`);
 });
 
-
-// https://www.movable-type.co.uk/scripts/latlong.html
-// the client sends a lat & long coordinates
-// it receives back an array af aircraft within the vicitity
-// the client calculates the bearing from itself to each aircraft
-// it then takes a compass bearing and calculates which
-// aircraft is the most likely for it to be pointing at
-// it shows this to the user, as well as the distance to the
-// aircraft (taking height of the aircraft into account).
-// at the bottom of the page it says something like
-// 'not the plane you were expecting? see x other planes nearby'
-// pressing that will pull up a list of nearby planes, their bearing
-// and distance and all associated info
-// there should also be a refresh button somewhere to receive up-to-date info
